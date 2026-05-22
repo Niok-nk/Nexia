03c4d8402ca5eea1ecc2f54bc6dd877843627989
Niok-nk <0499niok@gmail.com>
2026-05-21 19:52:56 -0500

    chore: remove temp check file

--- apps/backend/_check.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const p = new PrismaClient();
-p.userData.findMany().then(r => {
-  console.log('UserData count:', r.length);
-  console.log('Rows:', JSON.stringify(r, null, 2));
-}).catch(e => {
-  console.error('Error:', e.message);
-}).finally(() => p.$disconnect());
